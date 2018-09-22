441100391012aa42ef7d9ccf2ca6b495a5d2e375
Jacob Spade <jacobspade@Jacobs-MacBook-Pro.local>
2018-09-22 14:17:39 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Posts from './components/Posts.js';
+import PostForm from './components/Postform.js';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <PostForm />
+        <hr />
+        <Posts />
+      </div>
+    );
+  }
+}
+
+export default App;
