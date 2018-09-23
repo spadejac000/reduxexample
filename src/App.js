531fee670bde5252bb831ce41e1e4f87635119c2
Jacob Spade <jacobspade@Jacobs-MacBook-Pro.local>
2018-09-23 10:18:25 -0700

    added posting functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Posts from './components/Posts.js';
-import PostForm from './components/Postform.js';
+import {Provider} from 'react-redux';
+
+import Posts from './components/Posts';
+import PostForm from './components/Postform';
+
+import store from './store'
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <PostForm />
-        <hr />
-        <Posts />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <PostForm />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
